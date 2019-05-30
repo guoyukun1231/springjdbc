823454c2c690f1b66e5c649e9c527f6bc16e095f
Xiao-Kun <Xiao-Kun@192.168.127.76>
2019-05-30 15:14:26 +0800

    aaaaa

--- test001/src/test001/test01.java
+++ test001/src/test001/test01.java
@@ -3,5 +3,9 @@ package test001;
 public class test01 {
 public static void main(String[] args) {
 	System.out.println("test");
+	System.out.println("test");
+	System.out.println("test");
+	System.out.println("test");
+	System.out.println("test");
 }
 }
